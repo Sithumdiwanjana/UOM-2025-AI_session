f169503b0822fdcc9c038397fd2417a41594a324
Sithumdiwanjana <sithumdiwanjana32@gmail.com>
2025-10-07 13:27:38 +0530

    eular1

---
+++ eular1.py
@@ -0,0 +1,14 @@
+def sum_of_multiples(limit):
+    """
+    Calculate the sum of all multiples of 3 or 5 below the given limit.
+
+    :param limit: The upper limit (exclusive) to consider for multiples.
+    :return: The sum of all multiples of 3 or 5 below the limit.
+    """
+    total_sum = 0
+    for number in range(limit):
+        if number % 3 == 0 or number % 5 == 0:
+            total_sum += number
+    return total_sum
+
+print(sum_of_multiples(1000))
